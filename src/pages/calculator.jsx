a92d2badfb6fbbdf8a20dc4402e29de554fb70f7
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-27 22:56:45 -0800

    Create a file to implement a calculator

---
+++ src/pages/calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Calculator = () => {
+
+}
+
+const NumberButton = (props) => {
+  return (
+    <button>{props.value}</button>
+  )
+} 
+
+export default Calculator;
