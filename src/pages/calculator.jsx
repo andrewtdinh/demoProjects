2aa84ef15bf72820a10220cc4d6a2a400d9b3670
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-29 23:44:11 -0800

    Remove css module because not using plugin

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import Calculator from '../projects/calculator';
 
-
-
 export default () => <Calculator />
--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 const Calculator = () => {
   return (
     <div className="container">
