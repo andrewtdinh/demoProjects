b4990a78ad96024fac0d41a5617f79818ad77509
Andrew Dinh <andrewdinh74@yahoo.com>
2020-02-03 18:49:14 -0800

    Working out the calculator display

--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Styles from './index.module.css';
 
+const displayBGColor = '#918987';
+const displayColor = '#ffffff';
+
 const Calculator = () => {
   return (
     <div className={Styles.container} >
-      <div className={Styles.calcTitle} >imMaculator</div>
-      <div className={Styles.calcWrapper}></div>
+      <div className={Styles.calcTitle}>imMaculator</div>
+      <div className={Styles.calcWrapper}>
+
+      </div>
     </div>
   )
 }
 
-const Display = () => {
-  return (
-    <div className="displayContainer">
-
-    </div>
-  )
+const Display = ({ bgColor=displayBGColor, textColor=displayColor, strValue }) => {
+  return <div className="displayContainer">{strValue}</div>
 }
 
 const Button = (props) => {
