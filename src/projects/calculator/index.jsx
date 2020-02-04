818c6697df16379a6060518e1af72a25357b1350
Andrew Dinh <andrewdinh74@yahoo.com>
2020-02-03 23:08:59 -0800

    Remove the border for the calculator wrapper

--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -9,18 +9,22 @@ const Calculator = () => {
     <div className={Styles.container} >
       <div className={Styles.calcTitle}>imMaculator</div>
       <div className={Styles.calcWrapper}>
-
+        <Display bgColor={displayBGColor} textColor={displayColor} />
       </div>
     </div>
   )
 }
 
-const Display = ({ bgColor=displayBGColor, textColor=displayColor, strValue }) => {
-  return <div className="displayContainer">{strValue}</div>
+const Display = ({ bgColor=displayBGColor, textColor=displayColor, strValue='' }) => {
+  return <div style={{
+    backgroundColor: bgColor,
+    color: displayColor,
+    gridColumn: 'span 4',
+  }}>{strValue}</div>
 }
 
 const Buttons = () => {
-  
+
 }
 
 const Button = (props) => {
