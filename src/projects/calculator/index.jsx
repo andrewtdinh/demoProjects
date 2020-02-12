120eca8c2224de7b5a071d561c54710ff0d303e2
Andrew Dinh <andrewdinh74@yahoo.com>
2020-02-12 00:22:22 -0800

    Move the Button component inside Calculator

--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -35,6 +35,34 @@ const Calculator = () => {
     )
   }
 
+  const Button = props => {
+    const { onClick, label, color, bgColor, span, name, type } = props
+    const fontSize =
+      type === "blue-button"
+        ? "3rem"
+        : type === "orange-operator-button"
+        ? "1.3rem"
+        : "0.9rem"
+
+    return (
+      <button
+        onClick={onClick ? onClick : () => {}}
+        style={{
+          gridColumn: span,
+          color: color,
+          backgroundColor: bgColor,
+          fontWeight: 600,
+          fontSize: fontSize,
+          borderRadius: "7px",
+        }}
+        key={name}
+        aria-label={name}
+      >
+        {label}
+      </button>
+    )
+  }
+
   const renderButtons = () => {
     return buttons.map(buttonProps => {
       const {
@@ -74,32 +102,6 @@ const Calculator = () => {
         {renderButtons()}
       </div>
     </div>
-  )
-}
-
-
-const Button = (props) => {
-  const { onClick, label, color, bgColor, span, name, type } = props;
-  const fontSize = type === 'blue-button' ? 
-    '3rem' : 
-    type === 'orange-operator-button' ? '1.3rem' : '0.9rem'
-
-  return (
-    <button 
-      onClick={onClick ? onClick : () => {}}
-      style={{
-        gridColumn: span,
-        color: color,
-        backgroundColor: bgColor,
-        fontWeight: 600,
-        fontSize: fontSize,
-        borderRadius: '7px'
-      }}
-      key={name}
-      aria-label={name}
-    >
-      {label}
-    </button>
   )
 }
 
