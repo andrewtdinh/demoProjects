c8f281caba189a904f0837e37a2269b91083805e
Andrew Dinh <andrewdinh74@yahoo.com>
2020-02-11 18:31:24 -0800

    Make some more tweaks to prevent buttons from breaking

--- src/projects/calculator/buttons.js
+++ src/projects/calculator/buttons.js
@@ -44,7 +44,7 @@ export const buttons = [
   },
   {
     name: "Sign Toggle Operator",
-    label: "＋ / –",
+    label: "±",
     color: whiteText,
     bgColor: orangeOperatorColor,
     type: "orange-operator-button",
--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -55,7 +55,7 @@ const Button = (props) => {
   const { onClick, label, color, bgColor, span, name, type } = props;
   const fontSize = type === 'blue-button' ? 
     '3rem' : 
-    type === 'orange-operator-button' ? '1.4rem' : '1.1rem'
+    type === 'orange-operator-button' ? '1.3rem' : '0.9rem'
 
   return (
     <button 
