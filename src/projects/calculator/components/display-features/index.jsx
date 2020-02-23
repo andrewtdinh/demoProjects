ea7ed4e3a963b8bf528510c47f9a7ac29eedfafc
Andrew Dinh <andrewdinh74@yahoo.com>
2020-02-23 00:01:24 -0800

    Duplicate Display component to DisplayFeatures component

---
+++ src/projects/calculator/components/display-features/index.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { displayBGColor, displayColor } from "../../buttons"
+import Styles from "../../index.module.css"
+
+const DisplayFeatures = ({
+  bgColor = displayBGColor,
+  textColor = displayColor,
+  displayStr = "0",
+}) => {
+  return (
+    <div
+      className={Styles.displayWrapper}
+      style={{
+        backgroundColor: bgColor,
+        color: textColor,
+      }}
+    >
+      <div className={Styles.displayWindow}>{displayStr}</div>
+    </div>
+  )
+}
+
+export default DisplayFeatures;
