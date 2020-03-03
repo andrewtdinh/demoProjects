024b483313f2da3ad7c01a2014cb3293824cc67a
Andrew Dinh <andrewdinh74@yahoo.com>
2020-03-02 22:27:13 -0800

    Copy ResultsBar component to ScrollBar

---
+++ src/projects/calculator/components/scrollbar/index.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { resultsBarBGColor, resultsTextColor } from "../../buttons"
+import Styles from "../../index.module.css"
+
+const ResultsBar = ({
+  bgColor = resultsBarBGColor,
+  textColor = resultsTextColor,
+  results = [],
+}) => {
+  return (
+    <div
+      className={Styles.resultsBar}
+      style={{
+        backgroundColor: bgColor,
+        color: textColor,
+      }}
+    >
+      <div className={Styles.resultsBarLabel}>Res:</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default ResultsBar
