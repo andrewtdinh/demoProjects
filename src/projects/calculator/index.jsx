a4f004f85e277e6750c0fa9e85de5f58a0bc27cd
Andrew Dinh <andrewd@benchling.com>
2020-03-04 23:52:59 -0800

    Remove scrollBar import calculator index

--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -5,7 +5,6 @@ import Display from './components/display';
 import DisplayFeatures from './components/display-features';
 import MemoryBar from './components/memory';
 import ResultsBar from './components/results';
-import ScrollBar from './components/scrollbar';
 import { 
   buttons, 
   displayColor, 
