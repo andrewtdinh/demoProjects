cb0acf351f51a5fd57be677e724755141ae947cc
Andrew Dinh <andrewd@benchling.com>
2020-03-14 23:49:02 -0700

    Set four constants relating to Results and Memory Bar

--- src/projects/calculator/constants.js
+++ src/projects/calculator/constants.js
@@ -1,3 +1,8 @@
 import { whiteText } from './buttons';
 export const scrollBarBGColor = "#6d6866"
 export const scrollBarTextColor = whiteText
+// Memory Bar and Results Bar
+export const maxResultsEntries = 10
+export const maxMemoryEntries = 10
+export const displayedResultsEntries = 4
+export const displayedMemoryEntries = 4
