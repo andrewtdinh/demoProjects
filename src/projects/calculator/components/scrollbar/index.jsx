2962a0fc080efdeb74475b41c056f0ee47b882dd
Andrew Dinh <andrewd@benchling.com>
2020-03-24 22:17:37 -0700

    Update before switching to work on something else for a while

--- src/projects/calculator/components/scrollbar/index.jsx
+++ src/projects/calculator/components/scrollbar/index.jsx
@@ -1,33 +1,35 @@
-import React from "react"
-import { scrollBarBGColor, scrollBarTextColor } from "../../constants"
-import Styles from "../../index.module.css"
-
+import React from 'react';
+import { scrollBarBGColor, scrollBarTextColor } from '../../constants';
+import Styles from '../../index.module.css';
 
 const ScrollBar = ({
-  bgColor=scrollBarBGColor,
-  textColor=scrollBarTextColor,
-  entries=[],
-  onShiftLeftClick,
-  onShiftRightClick,
+	bgColor = scrollBarBGColor,
+	textColor = scrollBarTextColor,
+	entries = [],
+	onShiftLeftClick,
+	onShiftRightClick
 }) => {
+	return (
+		<div
+			className={Styles.scrollBar}
+			style={{
+				backgroundColor: bgColor,
+				color: textColor
+			}}
+		>
+			<div className="shift-left" onClick={onShiftLeftClick}>
+				{'◀'}
+			</div>
+			<div className={Styles.scrollBarEntries}>
+				{entries.map((entry) => {
+					return <span key={entry}>{entry}</span>;
+				})}
+			</div>
+			<div className="shift-right" onClick={onShiftRightClick}>
+				{'▶'}
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div
-      className={Styles.scrollBar}
-      style={{
-        backgroundColor: bgColor,
-        color: textColor,
-      }}
-    >
-      <div className="shift-left" onClick={onShiftLeftClick} >{"◀"}</div>
-      <div className={Styles.scrollBarEntries}>
-        {entries.map(entry => {
-          return <span key={entry}>{entry}</span>
-        })}
-      </div>
-      <div className="shift-right" onClick={onShiftRightClick} >{"▶"}</div>
-    </div>
-  )
-}
-
-export default ScrollBar
+export default ScrollBar;
