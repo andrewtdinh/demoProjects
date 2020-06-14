6eb7a86e6181aee991da62b7d76746ca99da66d0
Andrew Dinh <andrewd@benchling.com>
2020-06-14 15:11:46 -0700

    Update results to have ten numbers

--- src/projects/calculator/components/results/index.jsx
+++ src/projects/calculator/components/results/index.jsx
@@ -8,7 +8,7 @@ import { getValuesFrom } from '../../utils/scrollbar';
 const ResultsBar = ({
   bgColor = resultsBarBGColor,
   textColor = resultsTextColor,
-  results = [1,2,3,4,5,6,7,8,9],
+  results = [0, 1,2,3,4,5,6,7,8,9],
 }) => {
   const { onResultsShiftLeftClick, onResultsShiftRightClick, resultsStartIndex } = useContext(AppContext);
   const displayedResults = getValuesFrom(results, resultsStartIndex);
