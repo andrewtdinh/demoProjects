6ae23c4311283510fc18d23e83d8f068fc3f980a
Andrew Dinh <andrewd@benchling.com>
2020-06-13 22:30:12 -0700

    Introduce an initial state for the calculator in CalculatorContext

--- src/projects/calculator/context/app-context.js
+++ src/projects/calculator/context/app-context.js
@@ -5,6 +5,22 @@ export const CalculatorContext = createContext();
 
 // Create a provider for components to consume and subscribe to changes
 export const CalculatorContextProvider = props => {
+  const initialState = {
+    displayStr: "0",
+    operand1: null,
+    operand2: null,
+    lastResult: null,
+    prevResults: [],
+    memories: [],
+    operation: null,
+    resultsStartIndex: 0,
+    memoryStartIndex: 0,
+    onResultsShiftLeftClick,
+    onResultsShiftRightClick,
+    onMemoryShiftLeftClick,
+    onMemoryShiftRightClick,
+  }
+  
   const [count, setCount] = useState(0)
 
   return (
