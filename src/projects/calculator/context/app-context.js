8cc3128168519298bee44e6b843a9734c24c8be1
Andrew Dinh <andrewd@benchling.com>
2020-06-16 00:38:33 -0700

    Redefine initial context for setContext

--- src/projects/calculator/context/app-context.js
+++ src/projects/calculator/context/app-context.js
@@ -23,8 +23,52 @@ const initialState = {
 }
 
 // Create Calculator Context
-export const CalculatorContext = createContext(initialState);
+export const CalculatorContext = createContext(initialState, () => {});
 
+// TODO: have the displayResults array in state and change it when people shift results left or right
+const onResultsShiftLeftClick = (e) => {
+  e.preventDefault()
+  console.log({currentResultsStartIdx})
+  return currentResultsStartIdx === 0
+    ? null
+    : setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx - 1});
+}
+
+const onResultsShiftRightClick = (e) => {
+  const { prevResults } = calcState;
+  const qtyPreviousResults = prevResults.length;
+  console.log({prevResults})
+
+  e.preventDefault()
+  return qtyPreviousResults < maxResultsEntries
+    ? currentResultsStartIdx < qtyPreviousResults - displayedResultsEntries
+      ? setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx + 1})
+      : null
+    : currentResultsStartIdx < maxResultsEntries - displayedResultsEntries
+      ? setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx + 1})
+      : null
+}
+
+const onMemoryShiftLeftClick = (e) => {
+  e.preventDefault()
+  return currentMemoryStartIdx === 0
+    ? null
+    : setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx - 1 });
+}
+
+const onMemoryShiftRightClick = (e) => {
+  const { memories } = calcState;
+  const qtyMemoryEntries = memories.length;
+
+  e.preventDefault()
+  return qtyMemoryEntries < maxMemoryEntries
+    ? currentMemoryStartIdx < qtyMemoryEntries - displayedMemoryEntries
+      ? setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx + 1 })
+      : null
+    : currentMemoryStartIdx < maxMemoryEntries - displayedMemoryEntries
+      ? setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx + 1 })
+      : null
+}
 // Create a provider for components to consume and subscribe to changes
 export const CalculatorContextProvider = props => {
 
@@ -33,51 +77,6 @@ export const CalculatorContextProvider = props => {
     resultsStartIndex: currentResultsStartIdx,
     memoryStartIndex: currentMemoryStartIdx,
   } = calcState;
-
-  // TODO: have the displayResults array in state and change it when people shift results left or right
-  const onResultsShiftLeftClick = (e) => {
-    e.preventDefault()
-    console.log({currentResultsStartIdx})
-    return currentResultsStartIdx === 0
-      ? null
-      : setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx - 1});
-  }
-
-  const onResultsShiftRightClick = (e) => {
-    const { prevResults } = calcState;
-    const qtyPreviousResults = prevResults.length;
-    console.log({prevResults})
-
-    e.preventDefault()
-    return qtyPreviousResults < maxResultsEntries
-      ? currentResultsStartIdx < qtyPreviousResults - displayedResultsEntries
-        ? setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx + 1})
-        : null
-      : currentResultsStartIdx < maxResultsEntries - displayedResultsEntries
-        ? setCalcState({ ...calcState, resultsStartIndex: currentResultsStartIdx + 1})
-        : null
-  }
-
-  const onMemoryShiftLeftClick = (e) => {
-    e.preventDefault()
-    return currentMemoryStartIdx === 0
-      ? null
-      : setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx - 1 });
-  }
-
-  const onMemoryShiftRightClick = (e) => {
-    const { memories } = calcState;
-    const qtyMemoryEntries = memories.length;
-
-    e.preventDefault()
-    return qtyMemoryEntries < maxMemoryEntries
-      ? currentMemoryStartIdx < qtyMemoryEntries - displayedMemoryEntries
-        ? setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx + 1 })
-        : null
-      : currentMemoryStartIdx < maxMemoryEntries - displayedMemoryEntries
-        ? setCalcState({ ...calcState, memoryStartIndex: currentMemoryStartIdx + 1 })
-        : null
-  }
 
   return (
     <CalculatorContext.Provider value={[ calcState, setCalcState ]}>
