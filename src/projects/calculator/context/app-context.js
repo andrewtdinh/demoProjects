e79ad032082443967118fcf462ecbff8702a35c9
Andrew Dinh <andrewd@benchling.com>
2020-06-15 23:33:55 -0700

    Use initialState when creating CalculatorContext

--- src/projects/calculator/context/app-context.js
+++ src/projects/calculator/context/app-context.js
@@ -6,26 +6,27 @@ import {
   displayedResultsEntries,
 } from "../constants";
 
-// Create Calculator Object
-export const CalculatorContext = createContext();
+const initialState = {
+  displayStr: "0",
+  operand1: null,
+  operand2: null,
+  lastResult: null,
+  prevResults: [],
+  memories: [],
+  operation: null,
+  resultsStartIndex: 0,
+  memoryStartIndex: 0,
+  onResultsShiftLeftClick,
+  onResultsShiftRightClick,
+  onMemoryShiftLeftClick,
+  onMemoryShiftRightClick,
+}
+
+// Create Calculator Context
+export const CalculatorContext = createContext(initialState);
 
 // Create a provider for components to consume and subscribe to changes
 export const CalculatorContextProvider = props => {
-  const initialState = {
-    displayStr: "0",
-    operand1: null,
-    operand2: null,
-    lastResult: null,
-    prevResults: [],
-    memories: [],
-    operation: null,
-    resultsStartIndex: 0,
-    memoryStartIndex: 0,
-    onResultsShiftLeftClick,
-    onResultsShiftRightClick,
-    onMemoryShiftLeftClick,
-    onMemoryShiftRightClick,
-  }
 
   const [ calcState, setCalcState ] = useState(initialState);
   const {
