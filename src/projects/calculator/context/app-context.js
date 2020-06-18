b347d58bb55b228996ac765de0b10e2561030600
Andrew Dinh <andrewd@benchling.com>
2020-06-18 01:05:32 -0700

    Simplify app-context

--- src/projects/calculator/context/app-context.js
+++ src/projects/calculator/context/app-context.js
@@ -1,26 +1,24 @@
-import React, { useState, createContext } from "react";
-import {
-  maxResultsEntries,
-  maxMemoryEntries,
-  displayedMemoryEntries,
-  displayedResultsEntries,
-} from "../constants";
+import React, { createContext } from "react";
+
+const initialState = {
+  displayStr: "0",
+  operand1: null,
+  operand2: null,
+  lastResult: null,
+  prevResults: [],
+  memories: [],
+  operation: null,
+  resultsStartIndex: 0,
+  memoryStartIndex: 0,
+};
 
 // Create Calculator Context
-export const CalculatorContext = createContext(initialState, () => {});
-
+export const CalculatorContext = createContext(initialState);
 
 // Create a provider for components to consume and subscribe to changes
 export const CalculatorContextProvider = props => {
-
-  const [ calcState, setCalcState ] = useState(initialState);
-  const {
-    resultsStartIndex: currentResultsStartIdx,
-    memoryStartIndex: currentMemoryStartIdx,
-  } = calcState;
-
   return (
-    <CalculatorContext.Provider value={[ calcState, setCalcState ]}>
+    <CalculatorContext.Provider value={initialState}>
       {props.children}
     </CalculatorContext.Provider>
   )
--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -14,18 +14,6 @@ import {
 import { CalculatorContext, CalculatorContextProvider } from './context/app-context';
 
 const Calculator = () => {
-  const initialState = {
-    displayStr: "0",
-    operand1: null,
-    operand2: null,
-    lastResult: null,
-    prevResults: [],
-    memories: [],
-    operation: null,
-    resultsStartIndex: 0,
-    memoryStartIndex: 0,
-  }
-  
   const [ calcState, setCalcState ] = useContext(CalculatorContext);
   
   const { 
