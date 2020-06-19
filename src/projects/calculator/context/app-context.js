767785ee91b90d3f73010957f7f2bd64c3b700a1
Andrew Dinh <andrewd@benchling.com>
2020-06-19 09:09:30 -0700

    Move hooks into the body of a function

--- src/projects/calculator/context/app-context.js
+++ src/projects/calculator/context/app-context.js
@@ -7,69 +7,71 @@ import {
 } from "../constants";
 
 
-const initialState = {
-  displayStr: "0",
-  operand1: null,
-  operand2: null,
-  lastResult: null,
-  prevResults: [],
-  memories: [],
-  operation: null,
-  resultsStartIndex: 0,
-  memoryStartIndex: 0,
-};
 
-
-const reducer = (state, action) => {
-  const {
-    resultsStartIndex: currentResultsStartIdx,
-    memoryStartIndex: currentMemoryStartIdx,
-    prevResults,
-    memories,
-  } = state;
-  const qtyPreviousResults = prevResults.length
-  const qtyMemoryEntries = memories.length
-
-  switch (action.type) {
-    case "RESULTS_SHIFT_LEFT":
-      return currentResultsStartIdx === 0
-        ? state
-        : { ...state, resultsStartIndex: currentResultsStartIdx - 1 }
-
-    case "RESULTS_SHIFT_RIGHT":
-      return qtyPreviousResults < maxResultsEntries
-        ? currentResultsStartIdx < qtyPreviousResults - displayedResultsEntries
-          ? { ...state, resultsStartIndex: currentResultsStartIdx + 1 }
-          : state
-        : currentResultsStartIdx < maxResultsEntries - displayedResultsEntries
-        ? { ...state, resultsStartIndex: currentResultsStartIdx + 1 }
-        : state
-
-    case "MEMORY_SHIFT_LEFT":
-      return currentMemoryStartIdx === 0
-        ? state
-        : { ...state, memoryStartIndex: currentMemoryStartIdx - 1 }
-
-    case "MEMORY_SHIFT_RIGHT":
-      return qtyMemoryEntries < maxMemoryEntries
-        ? currentMemoryStartIdx < qtyMemoryEntries - displayedMemoryEntries
-          ? { ...state, memoryStartIndex: currentMemoryStartIdx + 1 }
-          : state
-        : currentMemoryStartIdx < maxMemoryEntries - displayedMemoryEntries
-        ? { ...state, memoryStartIndex: currentMemoryStartIdx + 1 }
-        : state
-    
-    default: 
-      return state;
-  }
-};
-
-const [state, dispatch] = useReducer(reducer, initialState)
 // Create Calculator Context
 export const CalculatorContext = createContext();
 
 // Create a provider for components to consume and subscribe to changes
 export const CalculatorContextProvider = props => {
+  const initialState = {
+    displayStr: "0",
+    operand1: null,
+    operand2: null,
+    lastResult: null,
+    prevResults: [],
+    memories: [],
+    operation: null,
+    resultsStartIndex: 0,
+    memoryStartIndex: 0,
+  }
+
+  const reducer = (state, action) => {
+    const {
+      resultsStartIndex: currentResultsStartIdx,
+      memoryStartIndex: currentMemoryStartIdx,
+      prevResults,
+      memories,
+    } = state
+    const qtyPreviousResults = prevResults.length
+    const qtyMemoryEntries = memories.length
+
+    switch (action.type) {
+      case "RESULTS_SHIFT_LEFT":
+        return currentResultsStartIdx === 0
+          ? state
+          : { ...state, resultsStartIndex: currentResultsStartIdx - 1 }
+
+      case "RESULTS_SHIFT_RIGHT":
+        return qtyPreviousResults < maxResultsEntries
+          ? currentResultsStartIdx <
+            qtyPreviousResults - displayedResultsEntries
+            ? { ...state, resultsStartIndex: currentResultsStartIdx + 1 }
+            : state
+          : currentResultsStartIdx < maxResultsEntries - displayedResultsEntries
+          ? { ...state, resultsStartIndex: currentResultsStartIdx + 1 }
+          : state
+
+      case "MEMORY_SHIFT_LEFT":
+        return currentMemoryStartIdx === 0
+          ? state
+          : { ...state, memoryStartIndex: currentMemoryStartIdx - 1 }
+
+      case "MEMORY_SHIFT_RIGHT":
+        return qtyMemoryEntries < maxMemoryEntries
+          ? currentMemoryStartIdx < qtyMemoryEntries - displayedMemoryEntries
+            ? { ...state, memoryStartIndex: currentMemoryStartIdx + 1 }
+            : state
+          : currentMemoryStartIdx < maxMemoryEntries - displayedMemoryEntries
+          ? { ...state, memoryStartIndex: currentMemoryStartIdx + 1 }
+          : state
+
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <CalculatorContext.Provider value={[state, dispatch]}>
       {props.children}
