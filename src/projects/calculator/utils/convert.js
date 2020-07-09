4692fb877e76136b1d127de31cab65ef2ba0c6a4
Andrew Dinh <andrewd@benchling.com>
2020-07-08 23:29:49 -0700

    Define convertToPercent util function

--- src/projects/calculator/utils/convert.js
+++ src/projects/calculator/utils/convert.js
@@ -9,3 +9,17 @@ export const convertToFraction = (percentStr) => {
     return parts[0].splice(firstPartLength - 2, 0, '.') + parts[1];
   }
 }
+
+export const convertToPercent = (fractionStr) => {
+  const parts = fractionStr.split('.');
+  const secondPartLength = parts[1].length;
+  if (secondPartLength === 0) {
+    return parts[0] + '00.0';
+  } else if (secondPartLength === 1) {
+    return parts[0] + parts[1] + '0.0';
+  } else if (secondPartLength === 2) {
+    return parts[0] + parts[1] + '.0';
+  } else {
+    return parts[0] + parts[1].splice(2, 0, '.')
+  }
+}
