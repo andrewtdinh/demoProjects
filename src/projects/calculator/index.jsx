a65e5b2636a34bef7bedbf1fc6fdef8c3a291567
Andrew Dinh <andrewd@benchling.com>
2020-08-03 22:06:47 -0700

    Update onNumbersClick to remove a case that never happens

--- src/projects/calculator/index.jsx
+++ src/projects/calculator/index.jsx
@@ -70,7 +70,6 @@ const Calculator = () => {
     const { displayStr: previousDisplayStr } = state;
     
     e.preventDefault();
-    if (buttonValue === '.' && previousDisplayStr.includes('.')) {return;}
     if (buttonValue === '0' && previousDisplayStr === '0') {return;}
     if (previousDisplayStr === '0' && '123456789'.includes(buttonValue)) {
       updateDisplay(buttonValue);
